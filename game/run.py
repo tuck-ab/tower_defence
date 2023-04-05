3dc06be3241167294efdd57c8e5e83dacdec3aed
tuck-ab <alext.5@hotmail.co.uk>
2023-04-05 17:13:09 +0100

    Added the start of pygame

--- game/run.py
+++ game/run.py
@@ -1,3 +1,13 @@
+import contextlib
+
+with contextlib.redirect_stdout(None):
+    import pygame
+
 def run():
     """Runs the game"""
-    print("Hello World!")
+    ## Initialise pygame
+    pygame.init()
+
+    ## Create the screen and the clock
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
