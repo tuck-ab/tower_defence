288df1de76e7131ae6f48770f2453613210c83ea
tuck-ab <alext.5@hotmail.co.uk>
2023-04-07 22:12:42 +0100

    Improved enemy callback and added todo list

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Tower Defence
 
 My tower defence game in python using `pygame`
+
+## TO-DO
+
+* Spawner for the enemies
+* Slots for the towers
+* Base tower
+* Different enemy types
+* Different tower types
+* Puzzles
--- game/game.py
+++ game/game.py
@@ -9,16 +9,22 @@ class Game:
         
         self.tick = 0
         self.running = True
-        def enemy_callback(enemy):
-            self.running = False
         
-        self.enemy = BaseEnemy(self.map, 3, enemy_callback)
+        self.enemies = []
         
     def update(self):
         """Update the game this main loop itteration"""
-        self.enemy.update()
+        for enemy in self.enemies:
+            enemy.update()
+            
+        if self.tick % 50 == 0:
+            self.enemies.append(BaseEnemy(self.map, 3, self.enemy_callback))
+            
+    def enemy_callback(self, enemy):
+        self.enemies.remove(enemy)
         
     def draw_debug(self, screen: Surface):
         """Draw a skeleton of what is on the screen"""
         self.map.draw_path_line(screen)
-        self.enemy.draw_box(screen)
+        for enemy in self.enemies:
+            enemy.draw_box(screen)
